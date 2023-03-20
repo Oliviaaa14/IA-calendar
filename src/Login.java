36969cc4cc5340e8447607b359d23e74e9c0ecd2
17Frazer_O <17Frazer_O@bexleygs.co.uk>
2023-03-20 11:42:31 +0000

    Initial commit

---
+++ src/Login.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Login {
+    Scanner input = new Scanner(System.in);
+      String username;
+      String password;
+
+      Scanner input = new Scanner(System.in);
+    System.out.println("Enter Username : ");
+    String username = input.next();
+
+    Scanner input2 = new Scanner(System.in);
+    System.out.println("Enter Password : ");
+    String password = input2.next();
+}
